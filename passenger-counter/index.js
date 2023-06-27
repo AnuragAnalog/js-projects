b3b30bb6319319067d230f39608089d8189e0e1a
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-06-27 16:35:46 -0400

    added the project

---
+++ passenger-counter/index.js
@@ -0,0 +1,21 @@
+//@ts-check
+
+let countEL = document.getElementById("count-el");
+let count = Number.parseInt(countEL.innerText, 10);
+
+let savedEL = document.getElementById("saved-el");
+let saved = "Previous entries: ";
+
+// Increment the count variable
+function increment() {
+    count = count + 1;
+    countEL.innerText = count;
+}
+
+// Save the count to the page
+function save() {
+    saved += count.toString() + " - ";
+    savedEL.textContent = saved;
+    count = 0;
+    countEL.innerText = count;
+}
