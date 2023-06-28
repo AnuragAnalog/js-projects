d674b72307140283e21aefc5ed60816dced83f6f
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-06-28 16:25:47 -0400

    added some functionality

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,14 +1,22 @@
 //@ts-check
 
+let drawCard = 0;
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard];
 let hasblackJack = false;
 let isAlive = true;
 let message = "";
 let sum = firstCard + secondCard;
 let messageEL = document.getElementById("message-el");
+let sumEL = document.querySelector("#sum-el");
+let cardsEL = document.querySelector("#cards-el");
 
 function startGame() {
+    renderGame();
+}
+
+function renderGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂";
     } else if (sum === 21) {
@@ -19,7 +27,20 @@ function startGame() {
         isAlive = false;
     }
 
+    cardsEL.textContent = "Cards: ";
+    cards.forEach((card) => {
+        cardsEL.textContent += card + ", ";
+    });
+
+    sumEL.textContent = "Sum: " + sum;
     messageEL.textContent = message;
+}
+
+function newCard() {
+    drawCard = 7;
+    cards.push(drawCard);
+    sum += drawCard;
+    renderGame();
 }
 
 console.log(message);
