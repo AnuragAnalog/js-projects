87dd352294aa6dbec49f8cdc050c9b6b18ebfff6
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-07-11 15:27:51 -0400

    added some files

--- get-cart/index.js
+++ get-cart/index.js
@@ -1,7 +1,7 @@
 //@ts-check
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://get-cart-default-rtdb.firebaseio.com/"
