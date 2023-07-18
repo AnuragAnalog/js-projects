47d5a49f1a76d86c5ddb1089a78e28e149d724f7
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-07-17 21:27:13 -0400

    added start over button

--- pradakshana/index.js
+++ pradakshana/index.js
@@ -1,15 +1,17 @@
 //@ts-check
 
-let display = false;
+let resetBtnEL = null, display = false;
 let inputEL = document.querySelector("#input-field");
 let startBtnEL = document.querySelector("#start-btn");
 let ulEL = document.querySelector("#list-ul");
+let divEL = document.querySelector(".main-container");
 
 startBtnEL.addEventListener("click", function () {
-    let count, done = 0;
+    let i, count, done = 0;
 
     if (inputEL.value === "" || display === true) {
         resetInput(inputEL);
+
         return ;
     } else {
         done = 0;
@@ -19,7 +21,7 @@ startBtnEL.addEventListener("click", function () {
     count = Number.parseInt(inputEL.value, 10);
     resetInput(inputEL);
 
-    for (let i = 0; i < count; i++) {
+    for (i = 0; i < count; i++) {
         let liEL = document.createElement("li");
         liEL.setAttribute("id", `item-${i + 1}`);
 
@@ -27,21 +29,44 @@ startBtnEL.addEventListener("click", function () {
         ulEL.append(liEL);
     }
 
-    for (let i = 0; i < count; i++) {
+    for (i = 0; i < count; i++) {
         let liEL = document.querySelector(`#item-${i + 1}`);
 
         liEL.addEventListener("click", function () {
-            liEL.setAttribute("style", "background-color: #ff0000; color: #ffffff;");
+            liEL.setAttribute("style", "background-color: #30c023; color: #ffffff;");
             done += 1;
             console.log(done);
+
             if (done === count) {
+                resetBtnEL = addButton(divEL);
                 display = false;
             }
         });
     }
 
+    if (resetBtnEL !== null) {
+        resetBtnEL.addEventListener("click", function () {
+            console.log("Reset button clicked");
+            ulEL.innerHTML = "";
+            done = 0;
+        });
+    }
+
     display = true;
 });
+
+function addButton(divElement) {
+    let button, resetButton;
+
+    button = document.createElement("button");
+    button.setAttribute("id", "reset-btn");
+    button.textContent = "Start Over";
+    divElement.append(button);
+
+    resetButton = document.querySelector("#reset-btn");
+
+    return resetButton;
+}
 
 function resetInput(inputElement) {
     inputElement.value = "";
