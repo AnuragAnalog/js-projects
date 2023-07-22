de7f35d421f24096e96e6447e877e994a3486bfc
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-07-22 09:18:34 -0400

    added some stuff

--- google-news/index.js
+++ google-news/index.js
@@ -1,1 +1,22 @@
 //@ts-check
+
+let apikey = "cf0188a55b2199a6004b1345cb0a989b";
+let inputEL = document.querySelector("#search");
+let buttonEL = document.querySelector("#search-btn");
+let pEL = document.querySelector("news")
+
+buttonEL.addEventListener("click", function() {
+    let articles;
+    let searchValue = inputEL.value;
+    let url = `https://gnews.io/api/v4/search?q=${searchValue}&lang=en&country=us&max=10&apikey=${apikey}`;
+
+    fetch(url).then(
+        response => response.json()
+        ).then(function () {
+            
+    });
+});
+
+function resetInput(inputElement) {
+    inputElement.value = "";
+}
