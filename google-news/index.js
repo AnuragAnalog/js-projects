c2945b93a3a2a57264442e21db51cb84ca144a26
AnuragAnalog <anurag.peddi1998@gmail.com>
2023-07-24 16:10:48 -0400

    updated ignore file and news app

--- google-news/index.js
+++ google-news/index.js
@@ -3,20 +3,33 @@
 let apikey = "cf0188a55b2199a6004b1345cb0a989b";
 let inputEL = document.querySelector("#search");
 let buttonEL = document.querySelector("#search-btn");
-let pEL = document.querySelector("news")
+let pEL = document.querySelector("#news");
+let ulEL = document.querySelector("#ul");
 
 buttonEL.addEventListener("click", function() {
     let articles;
     let searchValue = inputEL.value;
-    let url = `https://gnews.io/api/v4/search?q=${searchValue}&lang=en&country=us&max=10&apikey=${apikey}`;
+    let url = `https://gnews.io/api/v4/search?q=${searchValue}&lang=en&country=us&max=100&apikey=${apikey}`;
 
     fetch(url).then(
         response => response.json()
-        ).then(function () {
-            
+        ).then(function (data) {
+            articles = data.articles;
+            pEL.textContent = `There are a total of ${articles.length} articles on ${searchValue}`;
+
+            for (let i = 0; i < articles.length; i++) {
+                console.log(articles[i].title);
+            }
     });
 });
 
 function resetInput(inputElement) {
     inputElement.value = "";
+}
+
+function createLi(text) {
+    let li = document.createElement("li");
+    li.textContent = text;
+
+    return li;
 }
